9b5fcdb3acd50a2f306f8a4ae310cd01798a2dd1
Danil_Pak <dan.nid@mail.ru>
2024-12-08 22:40:53 +0500

    .

--- src/main/java/runner/TestRunner.java
+++ src/main/java/runner/TestRunner.java
@@ -2,7 +2,9 @@ package runner;
 
 
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.junit.platform.suite.api.*;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectDirectories;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
